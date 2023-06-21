05aa027e4dc919568998cce0a2736bd9a05c5638
DreamCat04 <112375774+DreamCat04@users.noreply.github.com>
2023-06-21 13:47:29 +0200

    Feat: Add async to function callbacks

--- src/app.ts
+++ src/app.ts
@@ -4,29 +4,29 @@ const app: Application = express();
 const port = 3000;
 app.use(express.json());
 
-app.get("/items", (request, response) => {
+app.get("/items", async (request, response) => {
     response.sendFile("/data.json");
 });
 
-app.get("/items/:id", (request, response) => {
+app.get("/items/:id", async (request, response) => {
     const itemToGet = request.params.id;
 });
 
-app.post("/items", (request, response) => {
+app.post("/items", async (request, response) => {
     //Wert entgegennehmen und ins JSON-file ablegen
 });
 
-app.patch("/items/:id", (request, response) => {
+app.patch("/items/:id", async (request, response) => {
     const itemToPatch = request.params.id;
     //Item mit der angegeben ID patchen
 });
 
-app.delete("/items/:id", (request, response) => {
+app.delete("/items/:id", async (request, response) => {
     const itemToDelete = request.params.id;
     //Item mit der angegebenen ID löschen
 });
 
-app.delete("/list", (request, response) => {
+app.delete("/list", async (request, response) => {
     //Eine ganze Liste Löschen
 });
 
