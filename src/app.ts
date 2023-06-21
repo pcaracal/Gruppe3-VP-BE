77a5b97bdd70f749e59b8c2559fb6adb00d1b988
DreamCat04 <112375774+DreamCat04@users.noreply.github.com>
2023-06-21 13:16:07 +0200

    Feat: implement HTTP endpoints

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,30 @@ const app: Application = express();
 const port = 3000;
 app.use(express.json());
 
+app.get("/items", (request, response) => {
+    response.sendFile("/data.json");
+});
 
+app.get("/items/:id", (request, response) => {
+    const itemToGet = request.params.id;
+});
 
+app.post("/items", (request, response) => {
+    //Wert entgegennehmen und ins JSON-file ablegen
+});
+
+app.patch("/items/:id", (request, response) => {
+    const itemToPatch = request.params.id;
+    //Item mit der angegeben ID patchen
+});
+
+app.delete("/items/:id", (request, response) => {
+    const itemToDelete = request.params.id;
+    //Item mit der angegebenen ID löschen
+});
+
+app.delete("/list", (request, response) => {
+    //Eine ganze Liste Löschen
+});
 
 app.listen(port, () => { console.log(`App is listening to: ${port}`); });
