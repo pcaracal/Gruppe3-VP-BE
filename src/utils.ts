c364a5d654052308f0557810373944f2951981e1
pcaracal <peter.schreivogel@stud.kbw.ch>
2023-06-21 09:09:14 +0200

    feat: get data from json function

--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,26 @@
-export interface Item{
-    id: number;
-    title: string;
-    status: boolean;
-    created_at: boolean;
-    fk_user_id: number;
+import fs from 'fs';
+
+export interface Item {
+  id: number;
+  title: string;
+  status: boolean;
+  created_at: number;
+  fk_user_id: number;
 }
 
-export interface Data{
-    items: Item[];
-    next_item_id: number;
+export interface Data {
+  items: Item[];
+  next_item_id: number;
 }
+
+export const getData = async () => {
+  return new Promise<Data>((resolve, reject) => {
+    fs.readFile('./data.json', (err: NodeJS.ErrnoException | null, buffer: Buffer) => {
+      if (err) reject("Get data unsuccessful.");
+      else {
+        resolve(JSON.parse(buffer.toString()));
+      }
+    });
+  });
+}
+
