46fa0aace878af46e939f650ff8fbb12b5749a97
DreamCat04 <112375774+DreamCat04@users.noreply.github.com>
2023-06-21 14:53:28 +0200

    Feat: Add functions to endpoints

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,35 @@
 import { Application, Request, Response } from 'express';
 import express from 'express';
 import './utils';
+import { addItem, deleteItemById, getData, putHandler } from './utils';
+import { it } from 'node:test';
 const app: Application = express();
 const port = 3000;
 app.use(express.json());
 
-app.get("/items", async (request, response) => {
+app.get("/items", (request, response) => {
+    getData();
 });
 
-app.get("/items/:id", async (request, response) => {
-    const itemToGet = request.params.id;
+app.post("/items", (request, response) => {
+    const dataToPost = request.body;
+    addItem(dataToPost);
+    response.status(201).send("Successfully added the item");
 });
 
-app.post("/items", async (request, response) => {
-    //Wert entgegennehmen und ins JSON-file ablegen
+app.patch("/items/:id", (request, response) => {
+    const itemToPatch = parseInt(request.params.id);
+    const dataToPatchWith = request.body;
+    putHandler(itemToPatch, dataToPatchWith);
 });
 
-app.patch("/items/:id", async (request, response) => {
-    const itemToPatch = request.params.id;
-    //Item mit der angegeben ID patchen
+app.delete("/items/:id", (request, response) => {
+    const itemToDelete = parseInt(request.params.id);
+    deleteItemById(itemToDelete);
 });
 
-app.delete("/items/:id", async (request, response) => {
-    const itemToDelete = request.params.id;
-    //Item mit der angegebenen ID löschen
-});
-
-app.delete("/list", async (request, response) => {
-    //Eine ganze Liste Löschen
+app.delete("/list", (request, response) => {
+    //implement saving userId into session
 });
 
 app.listen(port, () => { console.log(`App is listening to: ${port}`); });
