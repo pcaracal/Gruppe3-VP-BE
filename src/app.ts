f877555a16df9b8003f39160f587fa1ed6735305
pcaracal <peter.schreivogel@stud.kbw.ch>
2023-06-28 13:36:05 +0200

    feat: PATCH /items/:id requires login

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import { Application, Request, Response } from 'express';
 import express from 'express';
 import './utils';
 import session from 'express-session';
-import { Item, addItem, deleteItemById, getData, getItemsByUserId, patchHandler, users } from './utils';
+import { Item, addItem, deleteItemById, getData, getItems, getItemsByUserId, patchHandler, users } from './utils';
 const app: Application = express();
 const port = 3000;
 app.use(express.json());
@@ -60,22 +60,35 @@ app.post("/items", async (req: Request, res: Response) => {
     let dataToPost: Item = req.body;
     dataToPost.fk_user_id = req.session.user.id;
     await addItem(dataToPost);
-    res.send("Successfully added the item").status(201);
+
+    const catcat = await getData();
+    res.send(catcat.items.find(v => v.id === catcat.next_item_id - 1)).status(201);
   }
 });
 
-app.patch("/items/:id", (request, response) => {
-  const itemToPatch = parseInt(request.params.id);
-  const dataToPatchWith = request.body;
-  patchHandler(itemToPatch, dataToPatchWith);
+app.patch("/items/:id", async (req: Request, res: Response) => {
+  if (!req.session.user) res.sendStatus(401)
+  else {
+    const uid = req.session.user.id;
+    const catreal = await getItemsByUserId(uid);
+    if (!catreal.find(v => v.fk_user_id === uid && v.id === Number(req.params.id))) res.sendStatus(404);
+    else {
+      const patchId = parseInt(req.params.id);
+      const dataToPatchWith = req.body;
+      await patchHandler(patchId, dataToPatchWith);
+      res.send((await getItems()).find(v => v.id === patchId)).status(200);
+    }
+  }
 });
 
-app.delete("/items/:id", (request, response) => {
-  const itemToDelete = parseInt(request.params.id);
-  deleteItemById(itemToDelete);
+app.delete("/items/:id", async (req: Request, res: Response) => {
+  if (!req.session.user) res.sendStatus(401);
+
+  const itemToDelete = parseInt(req.params.id);
+  await deleteItemById(itemToDelete);
 });
 
-app.delete("/list", (request, response) => {
+app.delete("/list", async (req: Request, res: Response) => {
   //implement saving userId into session
 });
 
